05d6eef13da682b2276efb06e632de49ec580f79
Chris <cfretz@icloud.com>
2013-10-13 02:27:27 -0400

    initial drawing algo finished

--- Vanity/src/com/gmail/cfretz244/Vanity.java
+++ Vanity/src/com/gmail/cfretz244/Vanity.java
@@ -4,13 +4,18 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.URL;
 
 import javax.imageio.ImageIO;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.BlockState;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.material.MaterialData;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Vanity extends JavaPlugin {
@@ -28,27 +33,56 @@ public final class Vanity extends JavaPlugin {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(cmd.getName().equalsIgnoreCase("vanity")) {
 			if(sender instanceof Player) {
-				if(args.length == 2) {
-					Player player = (Player)sender;
-					int width = Integer.parseInt(args[0]), height = Integer.parseInt(args[1]);
-					try {
-						InputStream stream = this.getClass().getResourceAsStream("/pic.jpg");
-						BufferedImage img = ImageIO.read(stream);
-						img = resizeImage(img, width, height);
-						int[][] woolColors = processImage(img);
-					} catch(IOException e) {
-						//do something
-					} catch(Exception e) {
-						//do something
-					}
-				} else {
-					sender.sendMessage("Wrong number of parameters");
+				Player player = (Player)sender;
+				World world = player.getWorld();
+				int width = 150, height = 150;
+				Location loc;
+				loc = player.getLocation();
+//				if(args[0].equals("here")) {
+//					loc = player.getLocation();
+//				} else if(args[0].equals("there")) {
+//					loc = player.getTargetBlock(null, 200).getLocation();
+//				} else {
+//					loc = player.getLocation();
+//					double x = loc.getX();
+//					double y = loc.getY();
+//					double z = loc.getZ();
+//					loc = new Location(world, x - 1, y, z - 1);
+//				}
+				width = Integer.parseInt(args[0]);
+				height = Integer.parseInt(args[1]);
+				BufferedImage img;
+				if(args.length == 3) {
+					width = Integer.parseInt(args[1]);
+					height = Integer.parseInt(args[2]);
 				}
+				try {
+					URL url = new URL("https://scontent-a-lga.xx.fbcdn.net/hphotos-ash3/1234554_10151663409294037_1614676655_n.jpg");
+					img = ImageIO.read(url);
+				} catch(IOException e) {
+					player.sendMessage("Terribly sorry, but an error has occured. Perhaps the image does not exist?");
+					return false;
+				} catch(Exception e) {
+					player.sendMessage("Terribly sorry, but an error has occurred");
+					return false;
+				}
+				img = resizeImage(img, width, height);
+				int[][] woolColors = processImage(img);
+				writeBlocks(woolColors, loc);
 			}
 		} else {
 			sender.sendMessage("Wrong command");
 		}
 		return false;
+	}
+	
+	public BufferedImage resizeImage(BufferedImage img, int newWidth, int newHeight) {
+		int type = img.getType();
+		BufferedImage resizedImage = new BufferedImage(newWidth, newHeight, type);
+		Graphics2D g = resizedImage.createGraphics();
+		g.drawImage(img, 0, 0, newWidth, newHeight, null);
+		g.dispose();
+		return resizedImage;
 	}
 	
 	public int[][] processImage(BufferedImage img) {
@@ -59,37 +93,37 @@ public final class Vanity extends JavaPlugin {
 				int r = currentPixel.getRed();
 				int g = currentPixel.getGreen();
 				int b = currentPixel.getBlue();
-				if((r >= 200 && r <= 250) && (g >= 200 && g <= 250) && (b >= 200 && b <= 200)) {
+				if((r >= 190 && r <= 260) && (g >= 190 && g <= 260) && (b >= 190 && b <= 260)) {
 					pixelData[i][k] = 0;
-				} else if((r >= 140 && r <= 190) && (g >= 140 && g <= 190) && (b >= 140 && b <= 190)) {
+				} else if((r >= 130 && r <= 200) && (g >= 130 && g <= 200) && (b >= 130 && b <= 200)) {
 					pixelData[i][k] = 1;
-				} else if((r >= 40 && r <= 90) && (g >= 40 && g <= 90) && (b >= 40 && b <= 90)) {
+				} else if((r >= 30 && r <= 100) && (g >= 30 && g <= 100) && (b >= 30 && b <= 100)) {
 					pixelData[i][k] = 2;
-				} else if((r >= 0 && r <= 40) && (g >= 0 && g <= 40) && (b >= 0 && b <= 40)) {
+				} else if((r >= 0 && r <= 70) && (g >= 0 && g <= 70) && (b >= 0 && b <= 70)) {
 					pixelData[i][k] = 3;
-				} else if((r >= 135 && r <= 185) && (g >= 30 && g <= 80) && (b >= 30 && b <= 80)) {
+				} else if((r >= 125 && r <= 195) && (g >= 20 && g <= 90) && (b >= 20 && b <= 90)) {
 					pixelData[i][k] = 4;
-				} else if((r >= 195 && r <= 245) && (g >= 100 && g <= 150) && (b >= 35 && b <= 85)) {
+				} else if((r >= 185 && r <= 255) && (g >= 90 && g <= 160) && (b >= 25 && b <= 95)) {
 					pixelData[i][k] = 5;
-				} else if((r >= 165 && r <= 215) && (g >= 155 && g <= 205) && (b >= 15 && b <= 65)) {
+				} else if((r >= 155 && r <= 225) && (g >= 145 && g <= 215) && (b >= 5 && b <= 75)) {
 					pixelData[i][k] = 6;
-				} else if((r >= 40 && r <= 90) && (g >= 150 && g <= 200) && (b >= 35 && b <= 85)) {
+				} else if((r >= 30 && r <= 100) && (g >= 140 && g <= 210) && (b >= 25 && b <= 95)) {
 					pixelData[i][k] = 7;
-				} else if((r >= 35 && r <= 85) && (g >= 55 && g <= 105) && (b >= 5 && b <= 55)) {
+				} else if((r >= 25 && r <= 95) && (g >= 45 && g <= 115) && (b >= 0 && b <= 70)) {
 					pixelData[i][k] = 8;
-				} else if((r >= 65 && r <= 115) && (g >= 100 && g <= 150) && (b >= 170 && b <= 220)) {
+				} else if((r >= 55 && r <= 125) && (g >= 90 && g <= 160) && (b >= 160 && b <= 230)) {
 					pixelData[i][k] = 9;
-				} else if((r >= 25 && r <= 75) && (g >= 90 && g <= 140) && (b >= 115 && b <= 165)) {
+				} else if((r >= 15 && r <= 85) && (g >= 80 && g <= 150) && (b >= 105 && b <= 175)) {
 					pixelData[i][k] = 10;
-				} else if((r >= 20 && r <= 70) && (g >= 30 && g <= 80) && (b >= 110 && b <= 160)) {
+				} else if((r >= 10 && r <= 80) && (g >= 20 && g <= 90) && (b >= 100 && b <= 170)) {
 					pixelData[i][k] = 11;
-				} else if((r >= 95 && r <= 145) && (g >= 35 && g <= 85) && (b >= 155 && b <= 205)) {
+				} else if((r >= 85 && r <= 155) && (g >= 25 && g <= 95) && (b >= 145 && b <= 215)) {
 					pixelData[i][k] = 12;
-				} else if((r >= 150 && r <= 200) && (g >= 45 && g <= 95) && (b >= 160 && b <= 210)) {
+				} else if((r >= 140 && r <= 210) && (g >= 35 && g <= 105) && (b >= 150 && b <= 220)) {
 					pixelData[i][k] = 13;
-				} else if((r >= 185 && r <= 235) && (g >= 115 && g <= 165) && (b >= 135 && b <= 185)) {
+				} else if((r >= 175 && r <= 245) && (g >= 105 && g <= 175) && (b >= 125 && b <= 195)) {
 					pixelData[i][k] = 14;
-				} else if((r >= 50 && r <= 100) && (g >= 25 && g <= 75) && (b >= 5 && b <= 5)) {
+				} else if((r >= 40 && r <= 110) && (g >= 15 && g <= 85) && (b >= 0 && b <= 70)) {
 					pixelData[i][k] = 15;
 				} else {
 					pixelData[i][k] = -1;
@@ -100,13 +134,57 @@ public final class Vanity extends JavaPlugin {
 		return pixelData;
 	}
 	
-	public BufferedImage resizeImage(BufferedImage img, int newWidth, int newHeight) {
-		int type = img.getType();
-		BufferedImage resizedImage = new BufferedImage(newWidth, newHeight, type);
-		Graphics2D g = resizedImage.createGraphics();
-		g.drawImage(img, 0, 0, newWidth, newHeight, null);
-		g.dispose();
-		return resizedImage;
+	public void writeBlocks(int[][] woolColors, Location loc) {
+		Location currentLoc = new Location(loc.getWorld(), loc.getX(), loc.getY() + woolColors[0].length, loc.getZ());
+		for(int i = 0; i < woolColors.length; i++) {
+			for(int k = 0; k < woolColors[i].length; k++) {
+				currentLoc = new Location(currentLoc.getWorld(), currentLoc.getX(), currentLoc.getY(), currentLoc.getZ() + 1);
+				BlockState blockState = currentLoc.getBlock().getState();
+				blockState.setType(Material.WOOL);
+				byte color = 0000;
+				int woolColor = woolColors[i][k];
+				if(woolColor == 0) {
+					color = 0;
+				} else if(woolColor == 1) {
+					color = 8;
+				} else if(woolColor == 2) {
+					color = 7;
+				} else if(woolColor == 3) {
+					color = 15;
+				} else if(woolColor == 4) {
+					color = 14;
+				} else if(woolColor == 5) {
+					color = 1;
+				} else if(woolColor == 6) {
+					color = 4;
+				} else if(woolColor == 7) {
+					color = 5;
+				} else if(woolColor == 8) {
+					color = 13;
+				} else if(woolColor == 9) {
+					color = 3;
+				} else if(woolColor == 10) {
+					color = 9;
+				} else if(woolColor == 11) {
+					color = 11;
+				} else if(woolColor == 12) {
+					color = 10;
+				} else if(woolColor == 13) {
+					color = 2;
+				} else if(woolColor == 14) {
+					color = 6;
+				} else if(woolColor == 15) {
+					color = 12;
+				} else {
+					color = 8;
+				}
+				MaterialData data = blockState.getData();
+				data.setData(color);
+				blockState.setData(data);
+				blockState.update(true);
+			}
+			currentLoc = new Location(currentLoc.getWorld(), currentLoc.getX(), currentLoc.getY() - 1, currentLoc.getZ() - woolColors[0].length);
+		}
 	}
 	
 	
